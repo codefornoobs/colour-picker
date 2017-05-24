0e6c9ec7c8220fd00463c99621ac8cf99b8ae9e4
Code for noobs <hello@codefornoobs.com>
2017-05-25 00:11:42 +0100

    created the interaction and user feedback

--- colour-picker/colour-picker/MainActivity.cs
+++ colour-picker/colour-picker/MainActivity.cs
@@ -1,18 +1,62 @@
 ﻿using Android.App;
-using Android.Widget;
+using Android.Graphics;
 using Android.OS;
+using Android.Widget;
 
 namespace colour_picker
 {
-    [Activity(Label = "colour_picker", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Colour Picker", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private SeekBar _sbRed, 
+            _sbGreen, 
+            _sbBlue;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
+
+            _sbRed = FindViewById<SeekBar>(Resource.Id.sbRed);
+            _sbGreen = FindViewById<SeekBar>(Resource.Id.sbGreen);
+            _sbBlue = FindViewById<SeekBar>(Resource.Id.sbBlue);
+
+            _sbRed.ProgressChanged += (s, e) => { SeekBarValueChanged(); };
+            _sbGreen.ProgressChanged += (s, e) => { SeekBarValueChanged(); };
+            _sbBlue.ProgressChanged += (s, e) => { SeekBarValueChanged(); };
+        }
+
+        void SeekBarValueChanged()
+        {
+            TextView txtColour = FindViewById<TextView>(Resource.Id.txtColour);
+            TextView txtRed = FindViewById<TextView>(Resource.Id.txtRed);
+            TextView txtGreen = FindViewById<TextView>(Resource.Id.txtGreen);
+            TextView txtBlue = FindViewById<TextView>(Resource.Id.txtBlue);
+
+            int red = _sbRed.Progress;
+            int green = _sbGreen.Progress;
+            int blue = _sbBlue.Progress;
+
+            txtRed.Text = $"{Resources.GetString(Resource.String.Red)}: {red}";
+            txtGreen.Text = $"{Resources.GetString(Resource.String.Green)}: {green}";
+            txtBlue.Text = $"{Resources.GetString(Resource.String.Blue)}: {blue}";
+
+            txtColour.SetBackgroundColor(Color.Rgb(red, green, blue));
+
+            string hex = $"#{red:X2}{green:X2}{blue:X2}";
+            txtColour.Text = hex;
+            ChangeTextColour();
+        }
+
+        void ChangeTextColour()
+        {
+            TextView txtColour = FindViewById<TextView>(Resource.Id.txtColour);
+
+            if (_sbBlue.Progress+_sbGreen.Progress+_sbRed.Progress>450)
+                txtColour.SetTextColor(Color.Black);
+            else
+                txtColour.SetTextColor(Color.WhiteSmoke);
         }
     }
 }
--- colour-picker/colour-picker/Resources/Resource.Designer.cs
+++ colour-picker/colour-picker/Resources/Resource.Designer.cs
@@ -60,14 +60,26 @@ namespace colour_picker
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int seekBar1 = 2131034113;
+			// aapt resource value: 0x7f050005
+			public const int sbBlue = 2131034117;
 			
-			// aapt resource value: 0x7f050000
-			public const int textView1 = 2131034112;
+			// aapt resource value: 0x7f050003
+			public const int sbGreen = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int sbRed = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int txtBlue = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int txtColour = 2131034118;
 			
 			// aapt resource value: 0x7f050002
-			public const int textView2 = 2131034114;
+			public const int txtGreen = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int txtRed = 2131034112;
 			
 			static Id()
 			{
@@ -101,8 +113,17 @@ namespace colour_picker
 			// aapt resource value: 0x7f040001
 			public const int ApplicationName = 2130968577;
 			
+			// aapt resource value: 0x7f040004
+			public const int Blue = 2130968580;
+			
+			// aapt resource value: 0x7f040003
+			public const int Green = 2130968579;
+			
 			// aapt resource value: 0x7f040000
 			public const int Hello = 2130968576;
+			
+			// aapt resource value: 0x7f040002
+			public const int Red = 2130968578;
 			
 			static String()
 			{
